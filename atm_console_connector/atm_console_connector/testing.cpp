fe3e8c04f58e4ee7da6197575638cb0434b1a691
tgd_nWaA <bohdan.buchok@ukma.edu.ua>
2022-11-03 15:08:32 +0200

    UniqTesing added

--- atm_console_connector/atm_console_connector/testing.cpp
+++ atm_console_connector/atm_console_connector/testing.cpp
@@ -1,58 +1,102 @@
-#include <stdlib.h>
-#include <iostream>
-#include <sstream>
-#include <stdexcept>
-/* uncomment for applications that use vectors */
-/*#include <vector>*/
 
-#include "mysql_connection.h"
+//
+//#include <QtCore/QCoreApplication>
+//
+//#include <stdlib.h>
+//#include <iostream>
+//
+//#include <memory>
+//
+//#include "mysql_connection.h"
+//#include <cppconn/driver.h>
+//#include <cppconn/exception.h>
+//#include <cppconn/prepared_statement.h>
+//
+///*
+//         The JDBC API throws three different exceptions:
+//
+//       - sql::MethodNotImplementedException (derived from sql::SQLException)
+//       - sql::InvalidArgumentException (derived from sql::SQLException)
+//       - sql::SQLException (derived from std::runtime_error)
+//*/
+//
+//
+////for demonstration only. never save your password in the code!
+//const std::string server = "tcp://automatedmachinetellerdb.cuccuzhaeiiv.eu-central-1.rds.amazonaws.com:3306";
+//const std::string username = "tgd_nWaA";
+//const std::string password = "tgd32tgdWA";
+//
+//int main(int argc, char* argv[])
+//{
+//    QCoreApplication a(argc, argv);
+//
+//    std::unique_ptr<sql::Connection> con;
+//
+//    std::unique_ptr<sql::Driver> driver;
+//
+//    try
+//    {
+//        driver = std::make_unique<sql::Driver>(get_driver_instance());
+//        con = driver->connect(server, username, password);
+//
+//        std::cout << "Successfully connected" << "\n";
+//    }
+//    catch (sql::SQLException& e)
+//    {
+//        std::cout << "Could not connect to server. Error message: " << e.what() << "\n";
+//        system("pause");
+//        exit(1);
+//    }
+//
+//    con->setSchema("atm");
+//
+//    std::uniequ<sql::PreparedStatement>  pstmt;
+//    std::auto_ptr< sql::ResultSet > res;
+//
+//    pstmt.reset(con->prepareStatement("CALL get_data()"));
+//    res.reset(pstmt->executeQuery());
+//
+//    for (;;)
+//    {
+//        while (res->next()) {
+//            cout << "Name: " << res->getString("Name")
+//                << " Population: " << res->getInt("Population")
+//                << endl;
+//        }
+//        if (pstmt->getMoreResults())
+//        {
+//            res.reset(pstmt->getResultSet());
+//            continue;
+//        }
+//        break;
+//    }
+//
+//    delete res;
+//    delete stmt;
+//    delete con;
+//
+//    system("pause");
+//    return a.exec();
+//}
 
-#include <cppconn/driver.h>
-#include <cppconn/exception.h>
-#include <cppconn/resultset.h>
-#include <cppconn/statement.h>
-#include <cppconn/prepared_statement.h>
 
-#define EXAMPLE_HOST "localhost"
-#define EXAMPLE_USER "worlduser"
-#define EXAMPLE_PASS "worldpass"
-#define EXAMPLE_DB "world"
 
-using namespace std;
 
-int main(int argc, const char** argv)
-{
-    string url(argc >= 2 ? argv[1] : EXAMPLE_HOST);
-    const string user(argc >= 3 ? argv[2] : EXAMPLE_USER);
-    const string pass(argc >= 4 ? argv[3] : EXAMPLE_PASS);
-    const string database(argc >= 5 ? argv[4] : EXAMPLE_DB);
+///*
+//     MySQL Connector/C++ throws three different exceptions:
 
-    cout << "Connector/C++ tutorial framework..." << endl;
-    cout << endl;
+//     - sql::MethodNotImplementedException (derived from sql::SQLException)
+//     - sql::InvalidArgumentException (derived from sql::SQLException)
+//     - sql::SQLException (derived from std::runtime_error)
+//   */
+//cout << "# ERR: SQLException in " << __FILE__;
+//cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+///* what() (derived from std::runtime_error) fetches error message */
+//cout << "# ERR: " << e.what();
+//cout << " (MySQL error code: " << e.getErrorCode();
+//cout << ", SQLState: " << e.getSQLState() << " )" << endl;
 
-    try {
+//return EXIT_FAILURE;
 
-        /* INSERT TUTORIAL CODE HERE! */
 
-    }
-    catch (sql::SQLException& e) {
-        /*
-          MySQL Connector/C++ throws three different exceptions:
 
-          - sql::MethodNotImplementedException (derived from sql::SQLException)
-          - sql::InvalidArgumentException (derived from sql::SQLException)
-          - sql::SQLException (derived from std::runtime_error)
-        */
-        cout << "# ERR: SQLException in " << __FILE__;
-        cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
-        /* what() (derived from std::runtime_error) fetches error message */
-        cout << "# ERR: " << e.what();
-        cout << " (MySQL error code: " << e.getErrorCode();
-        cout << ", SQLState: " << e.getSQLState() << " )" << endl;
-
-        return EXIT_FAILURE;
-    }
-
-    cout << "Done." << endl;
-    return EXIT_SUCCESS;
-}
