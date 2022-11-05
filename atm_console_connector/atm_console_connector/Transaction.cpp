bf6083d3c20956ddd4e009f6118644c66bf5c8df
tgd_nWaA <bohdan.buchok@ukma.edu.ua>
2022-11-05 11:08:18 +0200

    Transaction model redisigned

--- atm_console_connector/atm_console_connector/Transaction.cpp
+++ atm_console_connector/atm_console_connector/Transaction.cpp
@@ -2,21 +2,20 @@
 #include "Transaction.h"
 
 Transaction::Transaction(
-	size_t id, size_t mach_id,
-	size_t acc_from, size_t acc_to,
-	size_t t_sum, std::string t_date,
-	bool successful, std::string t_descript)
-	: id(id), mach_id(mach_id),
-	  acc_from(acc_from), acc_to(acc_to),
-	  t_sum(t_sum), t_date(t_date),
-	  successful(successful), t_descript(t_descript) {};
+	std::string num,
+	std::string acc_from,
+	std::string acc_to,
+	size_t sum, std::string date,
+	bool successful, std::string descript)
+	: num(num), acc_from(acc_from), acc_to(acc_to),
+	  sum(sum), date(date), successful(successful),
+	  descript(descript) {};
 
 std::ostream& operator<<(std::ostream& os, const Transaction& trans) {
 	return os
-		<< "Transaction id: " << trans.get_id() << "\n"
-		<< "Machine id: " << trans.get_machine_id() << "\n"
-		<< "Account from id: " << trans.get_account_from_id() << "\n"
-		<< "Account to id: " << trans.get_account_to_id() << "\n"
+		<< "Transaction number: " << trans.get_num() << "\n"
+		<< "Payer account number: " << trans.get_account_from() << "\n"
+		<< "Payee account number: " << trans.get_account_to() << "\n"
 
 		<< "Transaction sum: " << trans.get_sum() << "\n"
 		<< "Transaction date: " << trans.get_date() << "\n"
--- atm_console_connector/atm_console_connector/Transaction.h
+++ atm_console_connector/atm_console_connector/Transaction.h
@@ -9,36 +9,35 @@ public:
 	Transaction(void) = default;
 
 	Transaction(
-		size_t id, size_t mach_id,
-		size_t acc_from, size_t acc_to,
-		size_t t_sum, std::string t_date,
-		bool successful, std::string t_descript);
+		std::string num,
+		std::string acc_from,
+		std::string acc_to,
+		size_t sum, std::string date,
+		bool successful, std::string descript);
 
 	Transaction(const Transaction&) = default;
 	Transaction& operator=(const Transaction&) = default;
 
 	~Transaction() = default;
 
-	inline size_t get_id() const { return id; };
-	inline size_t get_machine_id() const { return mach_id; };
-	inline size_t get_account_from_id() const { return acc_from; };
-	inline size_t get_account_to_id() const { return acc_to; };
+	inline std::string get_num() const { return num; }
+	inline std::string get_account_from() const { return acc_from; };
+	inline std::string get_account_to() const { return acc_to; };
 
-	inline size_t get_sum() const { return t_sum; };
-	inline std::string get_date() const { return t_date; };
+	inline size_t get_sum() const { return sum; };
+	inline std::string get_date() const { return date; };
 	inline bool is_successful() const { return successful; };
-	inline std::string get_descript() const { return t_descript; }; //description
+	inline std::string get_descript() const { return descript; }; //description
 
 private:
-	size_t id;
-	size_t mach_id;
-	size_t acc_from;
-	size_t acc_to;
+	std::string num;
+	std::string acc_from;
+	std::string acc_to;
 
-	size_t t_sum;
-	std::string t_date;
+	size_t sum;
+	std::string date;
 	bool successful;
-	std::string t_descript;
+	std::string descript;
 };
 
 std::ostream& operator<<(std::ostream& os, const Transaction& off);
@@ -51,4 +50,4 @@ std::ostream& operator<<(std::ostream& os, const Transaction& off);
 //t_sum	int unsigned	NO
 //t_date	date	NO
 //successful	tinyint	NO
-//t_descript	tinytext	NO
+//descript	tinytext	NO
